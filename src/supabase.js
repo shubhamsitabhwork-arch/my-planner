c76a3b5dd2913a1047cf880fff299304d8c3905e
shubhamsitabhwork-arch <shubhamsitabh.work@gmail.com>
2026-06-28 22:15:34 +0530

    env vars

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://kqorbtwggcjalbpuhqbt.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtxb3JidHdnZ2NqYWxicHVocWJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2ODM3NjksImV4cCI6MjA5ODI1OTc2OX0.BJlrOH7MQ7LfLf5Zb4QX6UntTY86T_aPzqCDPQu81_c'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
